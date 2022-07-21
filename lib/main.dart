b7521c97c5b6600836df4634c6a705c4aa1f8a07
Ash Winter <ash_winter@hotmail.co.uk>
2022-07-21 20:59:25 +0100

    Added fixed orientation and todo for test

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(const BullsEyeApp());
+}
+
+class BullsEyeApp extends StatelessWidget {
+  const BullsEyeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+    return const MaterialApp(
       title: 'Bullseye',
       home: GamePage(),
-    ),
-  );
+    );
+  }
 }
 
 class GamePage extends StatefulWidget {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:js';
+import 'dart:math';
+
 import 'package:bullseye/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -31,5 +34,34 @@ void main() {
     final buttonTextFinder = find.text('Hit me');
 
     expect(buttonTextFinder, findsOneWidget);
+  });
+
+  testWidgets('Gamepage has a dialog after button press',
+      (WidgetTester tester) async {
+    Widget testWidget = const MediaQuery(
+        data: MediaQueryData(), child: MaterialApp(home: GamePage()));
+
+    await tester.pumpWidget(testWidget);
+    final button = find.text('Hit me');
+
+    await tester.tap(button);
+    await tester.pump();
+    final popUpText = find.text('This is my first pop-up!');
+    expect(popUpText, findsOneWidget);
+
+    final awesome = find.text('Awesome!');
+    await tester.tap(awesome);
+    await tester.pump();
+    expect(popUpText, findsNothing);
+  });
+
+  testWidgets('GamePage is in landscape only', (WidgetTester tester) async {
+    Widget testWidget = const MediaQuery(
+        data: MediaQueryData(), child: MaterialApp(home: GamePage()));
+
+    // ignore: todo
+    //TODO - set screen size to portrait and check orientattion
+
+    await tester.pumpWidget(testWidget);
   });
 }
